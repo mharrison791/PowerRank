709d5069af6bbdfaa5e6abd4b762a53daf6c56ed
Matt <m.harrison79@gmail.com>
2022-11-30 20:45:27 +0000

    Updated script to pull direct from Fbref using WorldfootballR Begun to import Advanced Summary Data

--- CreateTransformRank.R
+++ CreateTransformRank.R
@@ -2,18 +2,27 @@ library(tidyverse)
 library(lubridate)
 library(reactablefmtr)
 library(scales)
-#library(worldfootballR)
+library(worldfootballR)
 
 
 temppal <- c('#36a1d6', '#76b8de', '#a0bfd9', '#ffffff', '#d88359', '#d65440', '#c62c34')
-Season_H2H <- read_csv("CurrentSeason_Results.csv")
+temppal_rev <- c('#c62c34','#d65440', '#d88359', '#ffffff','#a0bfd9', '#76b8de', '#36a1d6')
+
+Season_H2H <- fb_match_results(country = "ENG", gender = "M", season_end_year = 2023, tier = "1st")
+epl_2023_urls <- fb_match_urls(country = "ENG", gender = "M", season_end_year = 2023, tier="1st")
+advanced_match_stats <- fb_advanced_match_stats(match_url = epl_2023_urls, stat_type = "summary", team_or_player = "team")
+
+advanced_match_stats_Columns <- advanced_match_stats%>%
+  select(Game_URL,Team,Home_Away, Sh, SoT, SCA_SCA, Prog_Passes)
 
 
 image_url <- 'https://images.fotmob.com/image_resources/logo/teamlogo/'
 
 df <- Season_H2H %>%
   drop_na(Wk)%>%
-  drop_na(Score)
+  drop_na(HomeGoals)%>%
+  mutate(Wk = as.numeric(Wk))%>%
+  arrange(Wk)
 
 icons_df <- df %>%
   distinct(Home)%>%
@@ -32,38 +41,28 @@ pivot_df <- df %>%
 
 pivot_df <- left_join(pivot_df,icons_df, by = c("Team"="Home"))
 
-pivot_df <- pivot_df %>%
-  mutate(Home_Goals_Actual = substr(Score, 1, 1))%>%
-  mutate(Away_Goals_Actual = substr(Score,3,3))%>%
-  rename("Home xG" = xG...6)%>%
-  rename("Away xG" = xG...8 )
-
-pivot_df$Home_Goals_Actual <- as.numeric(pivot_df$Home_Goals_Actual)
-pivot_df$Away_Goals_Actual <- as.numeric(pivot_df$Away_Goals_Actual)
 pivot_df$Date <- ymd(pivot_df$Date)
 
 pivot_df <- pivot_df %>%
-  mutate(Points = ifelse((Location == "Home" & Home_Goals_Actual > Away_Goals_Actual), 3, 
-                         ifelse(Location == "Away" & Away_Goals_Actual > Home_Goals_Actual,3,
-                                ifelse(Home_Goals_Actual == Away_Goals_Actual,1,0))))
+  mutate(Points = ifelse((Location == "Home" & HomeGoals > AwayGoals), 3, 
+                         ifelse(Location == "Away" & AwayGoals > HomeGoals,3,
+                                ifelse(HomeGoals == AwayGoals,1,0))))
 
 pivot_df <- pivot_df %>%
-  mutate(Actual_GD = ifelse(Location == "Home",Home_Goals_Actual - Away_Goals_Actual,
-                            Away_Goals_Actual - Home_Goals_Actual))%>%
-  mutate(x_GD = ifelse(Location == "Home",`Home xG` - `Away xG`,
-                       `Away xG` - `Home xG`))
+  mutate(Actual_GD = ifelse(Location == "Home",HomeGoals - AwayGoals,
+                            AwayGoals - HomeGoals))%>%
+  mutate(x_GD = ifelse(Location == "Home",`Home_xG` - `Away_xG`,
+                       `Away_xG` - `Home_xG`))
 
 pivot_df <- pivot_df %>%
-  mutate(Actual_GF = ifelse(Location == "Home",Home_Goals_Actual,
-                            Away_Goals_Actual))%>%
-  mutate(Actual_GA = ifelse(Location == "Home", Away_Goals_Actual,
-                            Home_Goals_Actual))
-
-pivot_df <- pivot_df %>%
-  mutate(Actual_xG = ifelse(Location == "Home",`Home xG`,
-                            `Away xG`))%>%
-  mutate(Actual_xGA = ifelse(Location == "Home", `Away xG`,
-                             `Home xG`))
+  mutate(Actual_GF = ifelse(Location == "Home",HomeGoals,
+                            AwayGoals))%>%
+  mutate(Actual_GA = ifelse(Location == "Home", AwayGoals,
+                            HomeGoals))%>%
+  mutate(Actual_xG = ifelse(Location == "Home",`Home_xG`,
+                            `Away_xG`))%>%
+  mutate(Actual_xGA = ifelse(Location == "Home", `Away_xG`,
+                             `Home_xG`))
 
 list_of_teams <- unique(pivot_df$Team)
 
@@ -93,7 +92,7 @@ ranking_df <- binded_df %>%
             Total_xGA = sum(Actual_xGA),
             Points = sum(Points),
             GD = sum(Actual_GD),
-            xGD = sum(x_GD),
+            xGD = round(sum(x_GD),1),
             Differential = sum(sum(Actual_GD-x_GD)))%>%
   mutate(Rank_GF = rank(desc(Total_GF), ties.method = 'min'))%>%
   mutate(Rank_GA = rank(Total_GA, ties.method = 'min'))%>%
@@ -117,7 +116,7 @@ ranking_df <- binded_df %>%
 #####Plot
 
 binded_df %>%
-  ggplot(aes(Wk))+
+  ggplot(aes(Wk,group = 1))+
   geom_line(aes(y = cum_Actual_Goals, color = "var0"))+
   geom_line(aes(y = cum_XGF, color = "var1"))+
   facet_wrap( ~ Team)+
@@ -132,7 +131,7 @@ table_df <- joined_df %>%
 
 reactable(
   table_df,
-  theme = fivethirtyeight(header_font_size = 11),
+  theme = fivethirtyeight(header_font_size = 10),
   pagination = FALSE,
   columnGroups = list(
     colGroup(name = "Actual", columns = c("Total_GF","Total_GA","GD")),
@@ -144,12 +143,12 @@ reactable(
                            maxWidth = 60),
     Team = colDef(name='',
                   align = 'center',
-                  maxWidth = 100),
+                  maxWidth = 150),
     iconurl = colDef(name = '',
                      maxWidth = 60,
                      cell = embed_img(
-                       height = 25,
-                       width = 25)
+                       height = 20,
+                       width = 20)
     ),
     Points = colDef(
       maxWidth = 60,
@@ -177,14 +176,14 @@ reactable(
       name = "GA",
       align = "center",
       style = color_scales(table_df,
-                           colors = temppal)
+                           colors = temppal_rev)
     ),
     Total_xGA = colDef(
       maxWidth = 60,
       name = "GA",
       align = "center",
       style = color_scales(table_df,
-                           colors = temppal)
+                           colors = temppal_rev)
     ),
     GD = colDef(
       maxWidth = 60,
@@ -204,6 +203,7 @@ reactable(
         name = "Goal Differential",
         align = "center",
         cell = data_bars(table_df,
+                         text_position = "outside-end",
                          fill_color = temppal,
                          number_fmt = number_format(accuracy = 0.1))
       )
